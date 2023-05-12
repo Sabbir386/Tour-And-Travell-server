0eb34414f7cc62a9ffc6609b9604aad12faad734
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2023-05-12 21:41:19 +0600

    user payment

--- index.js
+++ index.js
@@ -10,120 +10,14 @@ const { query } = require("express");
 
 const app = express();
 
-
-
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
-
-//sslcommerz init
-app.get('/ssl-request', (req, res) => {
-  const data = {
-      total_amount: 6600,
-      currency: 'EUR',
-      tran_id: 'REF123',
-      success_url: `${process.env.ROOT}/ssl-payment-success`,
-      fail_url:  `${process.env.ROOT}/ssl-payment-failure`,
-      cancel_url:  `${process.env.ROOT}/ssl-payment-cancel`,
-      ipn_url:  `${process.env.ROOT}/ssl-payment-ipn`,
-      shipping_method: 'Courier',
-      product_name: 'Computer.',
-      product_category: 'Electronic',
-      product_profile: 'general',
-      cus_name: 'Customer Name',
-      cus_email: 'cust@yahoo.com',
-      cus_add1: 'Dhaka',
-      cus_add2: 'Dhaka',
-      cus_city: 'Dhaka',
-      cus_state: 'Dhaka',
-      cus_postcode: '1000',
-      cus_country: 'Bangladesh',
-      cus_phone: '01711111111',
-      cus_fax: '01711111111',
-      ship_name: 'Customer Name',
-      ship_add1: 'Dhaka',
-      ship_add2: 'Dhaka',
-      ship_city: 'Dhaka',
-      ship_state: 'Dhaka',
-      ship_postcode: 1000,
-      ship_country: 'Bangladesh',
-      multi_card_name: 'mastercard',
-      value_a: 'ref001_A',
-      value_b: 'ref002_B',
-      value_c: 'ref003_C',
-      value_d: 'ref004_D'
-  };
-  const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD,false) //true for live default false for sandbox
-  sslcommer.init(data).then(data => {
-      //process the response that got from sslcommerz 
-      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-      if (data?.GatewayPageURL) {
-        return res.status(200).redirect(data?.GatewayPageURL);
-      }
-      else {
-        return res.status(400).json({
-          message: "Session was not successful"
-        });
-      }
-
-  });
-});
-app.post("/ssl-payment-success", async (req, res) => {
-
-  /** 
-  * If payment successful 
-  */
-
-  // return res.status(200).json(
-  //   {
-  //     data: req.body,
-  //     message: 'Payment success'
-  //   }
-  // );
-  return res.status(200).redirect('http://localhost:3000/');
-});
-app.post("/ssl-payment-fail", async (req, res) => {
-
-  /** 
-  * If payment failed 
-  */
-
-  return res.status(200).json(
-    {
-      data: req.body,
-      message: 'Payment failed'
-    }
-  );
-});
-app.post("/ssl-payment-cancel", async (req, res) => {
-
-  /** 
-  * If payment cancelled 
-  */
-
-  return res.status(200).json(
-    {
-      data: req.body,
-      message: 'Payment cancelled'
-    }
-  );
-});
-app.post("/ssl-payment-ipn", async (req, res) => {
-
-  /** 
-  * If payment cancelled 
-  */
-
-  return res.status(200).json(
-    {
-      data: req.body,
-      message: 'Payment cancelled'
-    }
-  );
-});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -132,10 +26,145 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const hotelsCollection = client.db("tour").collection("hotels");
     const bookingsCollection = client.db("tour").collection("bookings");
+    const usersCollection = client.db("tour").collection("users");
+
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+
+      const user = await usersCollection.findOne(query);
+
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "Forbiden Access" });
+      }
+      next();
+    };
+
+    app.get("/user-count", async (req, res) => {
+      const query = { status: "verified" };
+      const total = await bookingsCollection.find(query).count();
+      res.send({ userCount: total });
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/user", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await usersCollection.count(query);
+      res.send({ feedback: result });
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    //sslcommerz init
+    app.get("/ssl-request", async (req, res) => {
+      const id = req.query.infos;
+      const query = { _id: new ObjectId(id) };
+      const product = await bookingsCollection.findOne(query);
+      console.log(product);
+      const data = {
+        total_amount: product.amount,
+        currency: "BDT",
+        tran_id: 'product._id',
+        success_url: `http://localhost:3000/`,
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        cus_name: 'product.fname' ,
+        cus_email: 'product.email',
+        cus_add1: 'product.address',
+        cus_phone: 'product.phone',
+      };
+      const sslcommer = new SSLCommerzPayment(
+        process.env.STORE_ID,
+        process.env.STORE_PASSWORD,
+        false
+      ); //true for live default false for sandbox
+      sslcommer.init(data).then((data) => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        if (data?.GatewayPageURL) {
+          return res.status(200).redirect(data?.GatewayPageURL);
+        } else {
+          return res.status(400).json({
+            message: "Session was not successful",
+          });
+        }
+      });
+    });
+    app.post("/ssl-payment-success", async (req, res) => {
+      /**
+       * If payment successful
+       */
+
+      // return res.status(200).json(
+      //   {
+      //     data: req.body,
+      //     message: 'Payment success'
+      //   }
+      // );
+      return res.status(200).redirect("http://localhost:3000/");
+    });
+    app.post("/ssl-payment-fail", async (req, res) => {
+      /**
+       * If payment failed
+       */
+
+      return res.status(200).json({
+        data: req.body,
+        message: "Payment failed",
+      });
+    });
+    app.post("/ssl-payment-cancel", async (req, res) => {
+      /**
+       * If payment cancelled
+       */
+
+      return res.status(200).json({
+        data: req.body,
+        message: "Payment cancelled",
+      });
+    });
+    app.post("/ssl-payment-ipn", async (req, res) => {
+      /**
+       * If payment cancelled
+       */
+
+      return res.status(200).json({
+        data: req.body,
+        message: "Payment cancelled",
+      });
+    });
 
     app.get("/hotels", async (req, res) => {
       const query = {};
@@ -145,18 +174,39 @@ async function run() {
     app.get("/hotels/search", async (req, res) => {
       const name = req.query.name;
       const money = req.query.money;
-      const query = {
-        $or: [
-          { title: { $regex: new RegExp(name, "i") } },
-          { price: { $lte: money } },
-        ],
-      };
-      const result = await hotelsCollection.find(query).toArray();
-      res.send(result);
+      if(money === ''){
+        const query = {
+          $or: [
+            { title: { $regex: new RegExp(name, "i") } },
+            // { price: { $lte: parseInt(money) } },
+          ],
+        };
+        const result = await hotelsCollection.find(query).toArray();
+        res.send(result);
+      }else if(name === ''){
+        const query = {
+          $or: [
+            // { title: { $regex: new RegExp(name, "i") } },
+            { price: { $lte: parseInt(money) } },
+          ],
+        };
+        const result = await hotelsCollection.find(query).toArray();
+        res.send(result);
+      }else{
+        const query = {
+          $and: [
+            { title: { $regex: new RegExp(name, "i") } },
+            { price: { $lte: parseInt(money) } },
+          ],
+        };
+        const result = await hotelsCollection.find(query).toArray();
+        res.send(result);
+      }
+      
     });
     app.get("/hotels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { title: id };
+      const query = { _id: id };
       const booking = await hotelsCollection.findOne(query);
       res.send(booking);
     });
@@ -165,16 +215,36 @@ async function run() {
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.get("/informations/:id", async (req, res) => {
+      const email = req.params.id;
+      const query = {email: email};
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const product = req.body;
       const result = await bookingsCollection.insertOne(product);
       res.send(result);
     });
- 
-    
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "5h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
   } finally {
   }
 }
+
 run().catch(console.log);
 app.get("/", (req, res) => {
   res.send("server is running");
