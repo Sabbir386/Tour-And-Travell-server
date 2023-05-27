f59eba3115742e605b6b6e7bcc76ebe8d49ade2c
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2023-05-27 23:05:38 +0600

    full work

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     const hotelsCollection = client.db("tour").collection("hotels");
+    const roomsCollection = client.db("tour").collection("rooms");
     const bookingsCollection = client.db("tour").collection("bookings");
     const usersCollection = client.db("tour").collection("users");
 
@@ -60,7 +61,162 @@ async function run() {
       }
       next();
     };
+    app.get("/dashboard", async (req, res) => {
+      const email = req.query.email;
+      const userInfo = await usersCollection.findOne({ email: email });
+      if (userInfo.role === "admin") {
+        const query = {};
+        const paidQuery = { status: "paid" };
+        const dueQuery = { status: "unpaid" };
+        const hotelCount = await hotelsCollection.find(query).count();
+        const bookingCount = await bookingsCollection.find(query).count();
+        const paidTotal = await bookingsCollection
+          .aggregate([
+            {
+              $match: paidQuery,
+            },
+            {
+              $group: { _id: null, total: { $sum: "$amount" } },
+            },
+          ])
+          .toArray();
+        const dueTotal = await bookingsCollection
+          .aggregate([
+            {
+              $match: dueQuery,
+            },
+            {
+              $group: { _id: null, total: { $sum: "$amount" } },
+            },
+          ])
+          .toArray();
+        const userBooking = await bookingsCollection
+          .aggregate([
+            {
+              $lookup: {
+                from: "users",
+                localField: "email",
+                foreignField: "email",
+                as: "branch",
+              },
+            },
+            {
+              $unwind: "$branch",
+            },
+            {
+              $group: {
+                _id: "$branch",
+                total: { $count: {} },
+              },
+            },
 
+            // {
+            //   $group: { _id: "$user", total: { $count: {} } },
+            // },
+          ])
+          .toArray();
+        const hotelBooking = await bookingsCollection
+          .aggregate([
+            {
+              $group: { _id: "$hotel", total: { $count: {} } },
+            },
+          ])
+          .toArray();
+
+        res.send({
+          hotelCount: hotelCount,
+          bookingCount: bookingCount,
+          paidTotal: paidTotal[0].total,
+          dueTotal: dueTotal[0].total,
+          userBooking: userBooking,
+          hotelBooking: hotelBooking,
+        });
+      } else {
+        const query = {};
+        const paidQuery = { status: "paid", email: email };
+        const dueQuery = { status: "unpaid", email: email };
+        const hotelCount = await hotelsCollection.find(query).count();
+        const bookingCount = await bookingsCollection
+          .find({ email: email })
+          .count();
+        const paidTotal = await bookingsCollection
+          .aggregate([
+            { $match: { $and: [{ email: email }, { status: "paid" }] } },
+
+            {
+              $group: { _id: null, total: { $sum: "$amount" } },
+            },
+          ])
+          .toArray();
+        const dueTotal = await bookingsCollection
+          .aggregate([
+            { $match: { $and: [{ email: email }, { status: "unpaid" }] } },
+            {
+              $group: { _id: null, total: { $sum: "$amount" } },
+            },
+          ])
+          .toArray();
+        const userBooking = await bookingsCollection
+          .aggregate([
+            {
+              $match: { email: email },
+            },
+            {
+              $lookup: {
+                from: "users",
+                localField: "email",
+                foreignField: "email",
+                as: "branch",
+              },
+            },
+            {
+              $unwind: "$branch",
+            },
+            {
+              $group: {
+                _id: "$branch",
+                total: { $count: {} },
+              },
+            },
+
+            // {
+            //   $group: { _id: "$user", total: { $count: {} } },
+            // },
+          ])
+          .toArray();
+          let hotelBooking;
+          if (userInfo.role === "admin") {
+             hotelBooking = await bookingsCollection
+            .aggregate([
+              {
+                $group: { _id: "$hotel", total: { $count: {} } },
+              },
+            ])
+            .toArray();
+          }else{
+             hotelBooking = await bookingsCollection
+            .aggregate([
+              {
+                $match: { email: email },
+              },
+              {
+                $group: { _id: "$hotel", total: { $count: {} } },
+              },
+            ])
+            .toArray();
+          }
+        
+
+        res.send({
+          hotelCount: hotelCount,
+          bookingCount: bookingCount,
+          paidTotal: paidTotal[0]?.total,
+          dueTotal: dueTotal[0]?.total,
+          userBooking: userBooking,
+          hotelBooking: hotelBooking,
+        });
+      }
+    });
     app.get("/user-count", async (req, res) => {
       const query = { status: "verified" };
       const total = await bookingsCollection.find(query).count();
@@ -89,51 +245,77 @@ async function run() {
     //sslcommerz init
     app.get("/ssl-request", async (req, res) => {
       const id = req.query.infos;
+
       const query = { _id: new ObjectId(id) };
       const product = await bookingsCollection.findOne(query);
-      console.log(product);
-      const data = {
-        total_amount: product.amount,
-        currency: "BDT",
-        tran_id: 'product._id',
-        success_url: `http://localhost:3000/`,
-        fail_url: "http://localhost:3030/fail",
-        cancel_url: "http://localhost:3030/cancel",
-        ipn_url: "http://localhost:3030/ipn",
-        cus_name: 'product.fname' ,
-        cus_email: 'product.email',
-        cus_add1: 'product.address',
-        cus_phone: 'product.phone',
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          status: "paid",
+        },
       };
-      const sslcommer = new SSLCommerzPayment(
+      const result = await bookingsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+
+      const data = {
+        total_amount: parseInt(product.amount),
+        currency: "BDT",
+        tran_id: "product._id", // use unique tran_id for each api call
+        success_url: "https://tourtravel-51405.web.app/",
+        fail_url: "http://localhost:3000/fail",
+        cancel_url: "http://localhost:3000/cancel",
+        ipn_url: "http://localhost:3000/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: product.fname,
+        cus_email: product.email,
+        cus_add1: product.address,
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      const sslcz = new SSLCommerzPayment(
         process.env.STORE_ID,
         process.env.STORE_PASSWORD,
         false
-      ); //true for live default false for sandbox
-      sslcommer.init(data).then((data) => {
-        //process the response that got from sslcommerz
-        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-        if (data?.GatewayPageURL) {
-          return res.status(200).redirect(data?.GatewayPageURL);
-        } else {
-          return res.status(400).json({
-            message: "Session was not successful",
-          });
-        }
+      );
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.redirect(GatewayPageURL);
+        console.log("Redirecting to: ", GatewayPageURL);
       });
     });
+
     app.post("/ssl-payment-success", async (req, res) => {
       /**
        * If payment successful
        */
-
       // return res.status(200).json(
       //   {
       //     data: req.body,
       //     message: 'Payment success'
       //   }
       // );
-      return res.status(200).redirect("http://localhost:3000/");
+      // return res.status(200).redirect("http://localhost:3000/");
     });
     app.post("/ssl-payment-fail", async (req, res) => {
       /**
@@ -166,15 +348,36 @@ async function run() {
       });
     });
 
+    app.post("/hotels", async (req, res) => {
+      const hotels = req.body;
+      const result = await hotelsCollection.insertOne(hotels);
+      res.send(result);
+    });
     app.get("/hotels", async (req, res) => {
       const query = {};
       const result = await hotelsCollection.find(query).toArray();
       res.send(result);
     });
+    app.post("/rooms", async (req, res) => {
+      const rooms = req.body;
+      const result = await roomsCollection.insertOne(rooms);
+      res.send(result);
+    });
+    app.get("/rooms", async (req, res) => {
+      const query = {};
+      const result = await roomsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { hotelId: id };
+      const booking = await roomsCollection.find(query).toArray();
+      res.send(booking);
+    });
     app.get("/hotels/search", async (req, res) => {
       const name = req.query.name;
       const money = req.query.money;
-      if(money === ''){
+      if (money === "") {
         const query = {
           $or: [
             { title: { $regex: new RegExp(name, "i") } },
@@ -183,7 +386,7 @@ async function run() {
         };
         const result = await hotelsCollection.find(query).toArray();
         res.send(result);
-      }else if(name === ''){
+      } else if (name === "") {
         const query = {
           $or: [
             // { title: { $regex: new RegExp(name, "i") } },
@@ -192,7 +395,7 @@ async function run() {
         };
         const result = await hotelsCollection.find(query).toArray();
         res.send(result);
-      }else{
+      } else {
         const query = {
           $and: [
             { title: { $regex: new RegExp(name, "i") } },
@@ -202,11 +405,10 @@ async function run() {
         const result = await hotelsCollection.find(query).toArray();
         res.send(result);
       }
-      
     });
     app.get("/hotels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id };
+      const query = { _id: new ObjectId(id) };
       const booking = await hotelsCollection.findOne(query);
       res.send(booking);
     });
@@ -218,9 +420,16 @@ async function run() {
 
     app.get("/informations/:id", async (req, res) => {
       const email = req.params.id;
-      const query = {email: email};
-      const result = await bookingsCollection.find(query).toArray();
-      res.send(result);
+      const userInfo = await usersCollection.findOne({ email: email });
+      if (userInfo.role === "admin") {
+        const query = {};
+        const result = await bookingsCollection.find(query).toArray();
+        res.send(result);
+      } else {
+        const query = { email: email };
+        const result = await bookingsCollection.find(query).toArray();
+        res.send(result);
+      }
     });
 
     app.post("/bookings", async (req, res) => {
@@ -240,6 +449,13 @@ async function run() {
         return res.send({ accessToken: token });
       }
       res.status(403).send({ accessToken: "" });
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
     });
   } finally {
   }
